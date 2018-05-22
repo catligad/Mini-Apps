44ac6d7a02f82cb826ee54408b057d4af258f87e
Catherine Ligad <kateligad@gmail.com>
2018-05-22 13:33:54 -0700

    set up server to handle post requests

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,27 +1,22 @@
 $(document).ready(function(){
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   $('#formSubmit').keypress(function(e) {
     if (e.which === 13) {
-      $('form#login').submit();
-      console.log($('#formSubmit').val())
+      $('#formSubmit').submit();
       return false;
     }
+  });
+
+  $('#formSubmit').submit((event) => {
+    const input =  $('#formSubmit').val();
+    console.log(input);
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:3000/dataIncoming",
+      contentType: "application/json",
+      data: input
+    }).done(function(data) {
+      console.log(data)
+    });
   })
+
 });
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,23 @@
+// You may assume the JSON data has a regular structure and hierarchy (see included sample file). The server must flatten the JSON hierarchy, mapping each item in the JSON to a single line of CSV report (see included sample output). You may assume child records in the JSON will always be in a property called children but you may not assume a JSON record has any other specific properties; i.e. any properties that exist besides children must be mapped to a column in your CSV report.
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+
 var app = express();
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.post('/dataIncoming', function (req, res) {
+  let keys = Object.keys(req);
+  console.log(keys)
+  
+  
+
+
+  res.send('I got it boo');
+})
 
 app.listen(3000, () => console.log('Listening on port 3000'));
