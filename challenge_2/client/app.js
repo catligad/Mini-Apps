6ddc59285d4aa203f9ed04f3da705c84d3fa8ada
Catherine Ligad <kateligad@gmail.com>
2018-05-22 11:44:33 -0700

    styled page and can submit text through a form

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
   $('#formSubmit').keypress(function(e) {
     if (e.which === 13) {
       $('form#login').submit();
+      console.log($('#formSubmit').val())
       return false;
     }
   })
