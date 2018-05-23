eb0fff87841fe59ef9b09547fe4f2233e80c43c6
Catherine Ligad <kateligad@gmail.com>
2018-05-23 15:07:37 -0700

    finished 3/4 pages

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -6,19 +6,10 @@ class App extends React.Component{
       firstPage: false,
       secondPage: false,
       thirdPage: false,
-      name:'',
-      email:'',
-      password:'',
-      addressLine1:'',
-      addressLine2:'',
-      city:'',
-      state:'',
-      zipCode:'',
-      phoneNumber:'',
-      creditCard:'',
-      expiryDate:'',
-      cvv:'',
-      billingZC:''
+      confirmationPage: false,
+      firstPageState:{},
+      secondPageState:{},
+      thirdPageState:{}
     };
   }
 
@@ -28,17 +19,65 @@ class App extends React.Component{
       firstPage:true,
     });
   }
+
+  onFirstPageClick(btn, state){
+    if (btn == 'previousBtn') {
+      this.setState({
+        checkoutPage: true,
+        firstPage:false,
+      });
+    } else if (btn == 'nextBtn') {
+      this.setState({
+        firstPage:false,
+        secondPage:true,
+        firstPageState: state,
+      });
+      console.log(this.state);
+    }
+  }
+
+  onSecondPageClick(btn, state){
+    if (btn == 'previousBtn') {
+      this.setState({
+        firstPage: true,
+        secondPage:false,
+      });
+    } else if (btn == 'nextBtn') {
+      this.setState({
+        secondPage:false,
+        thirdPage:true,
+        secondPageState: state,
+      });
+    }
+  }
+
+  onThirdPageClick(btn, state){
+    if (btn == 'previousBtn') {
+      this.setState({
+        secondPage: true,
+        thirdPage:false,
+      });
+    } else if (btn == 'nextBtn') {
+      this.setState({
+        thirdPage:false,
+        confirmationPage:true,
+        thirdPageState: state,
+      });
+    }
+  }
   
   render(){
     var pageRendered;
     if (this.state.checkoutPage) {
       pageRendered = <CheckoutPage click={this.onCheckoutClick.bind(this)} />
     } else if (this.state.firstPage) {
-      pageRendered = <FirstPage />
+      pageRendered = <FirstPage click={this.onFirstPageClick.bind(this)} />
     } else if (this.state.secondPage) {
-      pageRendered = <SecondPage />
+      pageRendered = <SecondPage click={this.onSecondPageClick.bind(this)} />
     } else if (this.state.thirdPage) {
-      pageRendered = <Thirdpage />
+      pageRendered = <ThirdPage click={this.onThirdPageClick.bind(this)} />
+    } else if (this.state.confirmationPage) {
+      pageRendered = <ConfirmationPage firstPageState={this.state.firstPageState} secondPageState={this.state.secondPageState} thirdPageState={this.state.thirdPageState} />
     }
 
     return(
@@ -49,39 +88,258 @@ class App extends React.Component{
   }
 }
 
+
+// ------------------------------------------------------------
 function CheckoutPage (props) {
-  function clickHandler(){
-    console.log('I was clicked!')
+  function handleClick(){
     return props.click();
   };
   return(
-    <div className="checkout" onClick={clickHandler.bind(this)}>
+    <div className="checkout" onClick={handleClick.bind(this)}>
     Checkout
     </div>
   )
 };
 
-function FirstPage (){
-  return(
+// -------------------------------------------------------------
+class FirstPage extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'',
+      email:'',
+      password:'',
+    }
+  }
+
+  handleChange(event){
+    this.setState({
+      [event.target.className] : event.target.value
+    });
+  }
+
+  // handleClick(event) {
+  //   this.setState
+  // }
+
+  handleNavClick(event) {
+    console.log(this.state);
+    let state = this.state;
+    return this.props.click(event.target.className, state);
+  }
+
+
+  render() {
+    return(
     <div className="holderFirstPage">
-    <div className="name"> 
-      Name
+
+    <div className="css"> Name </div>
+    <div className="form">
+      <input type="text" className="name" onChange={this.handleChange.bind(this)}></input>
     </div>
-    <div className="email"> 
-      Email
+
+    <div className="css"> Email </div>
+    <div className="form">
+      <input type="text" className="email" onChange={this.handleChange.bind(this)}></input>
     </div>
-    <div className="password"> 
-      Password
+
+    <div className="css"> Password </div>
+    <div className="form">
+      <input type="text" className="password" onChange={this.handleChange.bind(this)}></input>
     </div>
-    <div className="password"> 
-      Previous
+
+    <div className="navigator">
+      <span className="previousBtn" onClick={this.handleNavClick.bind(this)}> Previous </span>
+      <span className="submitBtn"> Submit </span>
+      <span className="nextBtn" onClick={this.handleNavClick.bind(this)}> Next </span>
     </div>
-    <div className="password"> 
-      Next
+
     </div>
-    </div>
-  )
+  )} 
 }
+
+
+// ------------------------------------------------------------
+class SecondPage extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      addressLine1:'',
+      addressLine2:'',
+      city:'',
+      state:'',
+      zipCode:'',
+      phoneNumber:'',
+    }
+  }
+
+  handleChange(event){
+    this.setState({
+      [event.target.className] : event.target.value
+    });
+  }
+
+  // handleClick(event) {
+  //   this.setState
+  // }
+
+  handleNavClick(event) {
+    return this.props.click(event.target.className, this.state);
+  }
+
+  render() {
+    return(
+    <div className="holderSecondPage">
+
+    <div className="css"> Address </div>
+    <div className="form">
+      <input type="text" className="addressLine1" onChange={this.handleChange.bind(this)}></input>
+    </div>
+    <div className="form">
+      <input type="text" className="addressLine2" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="css"> City </div>
+    <div className="form">
+      <input type="text" className="city" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="css"> State </div>
+    <div className="form">
+      <input type="text" className="state" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="css"> Zip Code </div>
+    <div className="form">
+      <input type="text" className="zipCode" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="css"> Phone Number </div>
+    <div className="form">
+      <input type="text" className="phoneNumber" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="navigator">
+      <span className="previousBtn" onClick={this.handleNavClick.bind(this)}> Previous </span>
+      <span className="submitBtn" > Submit </span>
+      <span className="nextBtn" onClick={this.handleNavClick.bind(this)}> Next </span>
+    </div>
+
+    </div>
+  )} 
+}
+
+// -------------------------------------------------------------
+class ThirdPage extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      creditCard:'',
+      expiryDate:'',
+      cvv:'',
+      billingZC:''
+    }
+  }
+
+  handleChange(event){
+    this.setState({
+      [event.target.className] : event.target.value
+    });
+  }
+
+  // handleClick(event) {
+  //   this.setState
+  // }
+
+  handleNavClick(event) {
+    return this.props.click(event.target.className, this.state);
+  }
+
+
+  render() {
+    return(
+    <div className="holderThirdPage">
+
+    <div className="css"> Credit Card </div>
+    <div className="form">
+      <input type="text" className="creditCard" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="css"> Expiry Date </div>
+    <div className="form">
+      <input type="text" className="expiryDate" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="css"> CVV </div>
+    <div className="form">
+      <input type="text" className="cvv" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="css"> Billing Zip </div>
+    <div className="form">
+      <input type="text" className="billingZC" onChange={this.handleChange.bind(this)}></input>
+    </div>
+
+    <div className="navigator">
+      <span className="previousBtn" onClick={this.handleNavClick.bind(this)}> Previous </span>
+      <span className="submitBtn"> Submit </span>
+      <span className="nextBtn" onClick={this.handleNavClick.bind(this)}> Next </span>
+    </div>
+
+    </div>
+  )} 
+}
+
+
+// -------------------------------------------------------------
+function ConfirmationPage (props){
+  console.log(props);
+    return(
+    <div className="holderConfirmationPage">
+    
+    <div className="css"> Name </div>
+    <div className="cssConf"> {props.name} </div>
+
+    <div className="css"> Email </div>
+    <div className="cssConf"> {props.email} </div>
+
+    <div className="css"> Password </div>
+    <div className="cssConf"> {props.password} </div>
+
+    <div className="css"> Address </div>
+    <div className="cssConf"> {props.addressLine1} </div>
+    <div className="cssConf"> {props.addressLine2} </div>
+
+    <div className="css"> City </div>
+    <div className="cssConf"> {props.city} </div>
+
+    <div className="css"> State </div>
+    <div className="cssConf"> {props.state} </div>
+
+    <div className="css"> Zip </div>
+    <div className="cssConf"> {props.zipCode} </div>
+
+    <div className="css"> Phone Number </div>
+    <div className="cssConf"> {props.phoneNumber} </div>
+
+    <div className="css"> Credit Card Number </div>
+    <div className="cssConf"> {props.creditCard} </div>
+
+    <div className="css"> Expiry Date </div>
+    <div className="cssConf"> {props.expiryDate} </div>
+
+    <div className="css"> CVV </div>
+    <div className="cssConf"> {props.cvv} </div>
+
+    <div className="css"> Billing Zip </div>
+    <div className="cssConf"> {props.billingZC} </div>
+
+    </div>
+  ) 
+}
+
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- challenge_3/public/appCompiled.js
+++ challenge_3/public/appCompiled.js
@@ -2,6 +2,8 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -21,19 +23,10 @@ var App = function (_React$Component) {
       firstPage: false,
       secondPage: false,
       thirdPage: false,
-      name: '',
-      email: '',
-      password: '',
-      addressLine1: '',
-      addressLine2: '',
-      city: '',
-      state: '',
-      zipCode: '',
-      phoneNumber: '',
-      creditCard: '',
-      expiryDate: '',
-      cvv: '',
-      billingZC: ''
+      confirmationPage: false,
+      firstPageState: {},
+      secondPageState: {},
+      thirdPageState: {}
     };
     return _this;
   }
@@ -47,17 +40,68 @@ var App = function (_React$Component) {
       });
     }
   }, {
+    key: 'onFirstPageClick',
+    value: function onFirstPageClick(btn, state) {
+      if (btn == 'previousBtn') {
+        this.setState({
+          checkoutPage: true,
+          firstPage: false
+        });
+      } else if (btn == 'nextBtn') {
+        this.setState({
+          firstPage: false,
+          secondPage: true,
+          firstPageState: state
+        });
+        console.log(this.state);
+      }
+    }
+  }, {
+    key: 'onSecondPageClick',
+    value: function onSecondPageClick(btn, state) {
+      if (btn == 'previousBtn') {
+        this.setState({
+          firstPage: true,
+          secondPage: false
+        });
+      } else if (btn == 'nextBtn') {
+        this.setState({
+          secondPage: false,
+          thirdPage: true,
+          secondPageState: state
+        });
+      }
+    }
+  }, {
+    key: 'onThirdPageClick',
+    value: function onThirdPageClick(btn, state) {
+      if (btn == 'previousBtn') {
+        this.setState({
+          secondPage: true,
+          thirdPage: false
+        });
+      } else if (btn == 'nextBtn') {
+        this.setState({
+          thirdPage: false,
+          confirmationPage: true,
+          thirdPageState: state
+        });
+      }
+    }
+  }, {
     key: 'render',
     value: function render() {
       var pageRendered;
       if (this.state.checkoutPage) {
         pageRendered = React.createElement(CheckoutPage, { click: this.onCheckoutClick.bind(this) });
       } else if (this.state.firstPage) {
-        pageRendered = React.createElement(FirstPage, null);
+        pageRendered = React.createElement(FirstPage, { click: this.onFirstPageClick.bind(this) });
       } else if (this.state.secondPage) {
-        pageRendered = React.createElement(SecondPage, null);
+        pageRendered = React.createElement(SecondPage, { click: this.onSecondPageClick.bind(this) });
       } else if (this.state.thirdPage) {
-        pageRendered = React.createElement(Thirdpage, null);
+        pageRendered = React.createElement(ThirdPage, { click: this.onThirdPageClick.bind(this) });
+      } else if (this.state.confirmationPage) {
+        pageRendered = React.createElement(ConfirmationPage, { firstPageState: this.state.firstPageState, secondPageState: this.state.secondPageState, thirdPageState: this.state.thirdPageState });
       }
 
       return React.createElement(
@@ -71,46 +115,506 @@ var App = function (_React$Component) {
   return App;
 }(React.Component);
 
+// ------------------------------------------------------------
+
+
 function CheckoutPage(props) {
-  function clickHandler() {
-    console.log('I was clicked!');
+  function handleClick() {
     return props.click();
   };
   return React.createElement(
     'div',
-    { className: 'checkout', onClick: clickHandler.bind(this) },
+    { className: 'checkout', onClick: handleClick.bind(this) },
     'Checkout'
   );
 };
 
-function FirstPage() {
+// -------------------------------------------------------------
+
+var FirstPage = function (_React$Component2) {
+  _inherits(FirstPage, _React$Component2);
+
+  function FirstPage(props) {
+    _classCallCheck(this, FirstPage);
+
+    var _this2 = _possibleConstructorReturn(this, (FirstPage.__proto__ || Object.getPrototypeOf(FirstPage)).call(this, props));
+
+    _this2.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+    return _this2;
+  }
+
+  _createClass(FirstPage, [{
+    key: 'handleChange',
+    value: function handleChange(event) {
+      this.setState(_defineProperty({}, event.target.className, event.target.value));
+    }
+
+    // handleClick(event) {
+    //   this.setState
+    // }
+
+  }, {
+    key: 'handleNavClick',
+    value: function handleNavClick(event) {
+      console.log(this.state);
+      var state = this.state;
+      return this.props.click(event.target.className, state);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'holderFirstPage' },
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Name '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'name', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Email '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'email', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Password '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'password', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'navigator' },
+          React.createElement(
+            'span',
+            { className: 'previousBtn', onClick: this.handleNavClick.bind(this) },
+            ' Previous '
+          ),
+          React.createElement(
+            'span',
+            { className: 'submitBtn' },
+            ' Submit '
+          ),
+          React.createElement(
+            'span',
+            { className: 'nextBtn', onClick: this.handleNavClick.bind(this) },
+            ' Next '
+          )
+        )
+      );
+    }
+  }]);
+
+  return FirstPage;
+}(React.Component);
+
+// ------------------------------------------------------------
+
+
+var SecondPage = function (_React$Component3) {
+  _inherits(SecondPage, _React$Component3);
+
+  function SecondPage(props) {
+    _classCallCheck(this, SecondPage);
+
+    var _this3 = _possibleConstructorReturn(this, (SecondPage.__proto__ || Object.getPrototypeOf(SecondPage)).call(this, props));
+
+    _this3.state = {
+      addressLine1: '',
+      addressLine2: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      phoneNumber: ''
+    };
+    return _this3;
+  }
+
+  _createClass(SecondPage, [{
+    key: 'handleChange',
+    value: function handleChange(event) {
+      this.setState(_defineProperty({}, event.target.className, event.target.value));
+    }
+
+    // handleClick(event) {
+    //   this.setState
+    // }
+
+  }, {
+    key: 'handleNavClick',
+    value: function handleNavClick(event) {
+      return this.props.click(event.target.className, this.state);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'holderSecondPage' },
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Address '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'addressLine1', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'addressLine2', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' City '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'city', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' State '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'state', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Zip Code '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'zipCode', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Phone Number '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'phoneNumber', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'navigator' },
+          React.createElement(
+            'span',
+            { className: 'previousBtn', onClick: this.handleNavClick.bind(this) },
+            ' Previous '
+          ),
+          React.createElement(
+            'span',
+            { className: 'submitBtn' },
+            ' Submit '
+          ),
+          React.createElement(
+            'span',
+            { className: 'nextBtn', onClick: this.handleNavClick.bind(this) },
+            ' Next '
+          )
+        )
+      );
+    }
+  }]);
+
+  return SecondPage;
+}(React.Component);
+
+// -------------------------------------------------------------
+
+
+var ThirdPage = function (_React$Component4) {
+  _inherits(ThirdPage, _React$Component4);
+
+  function ThirdPage(props) {
+    _classCallCheck(this, ThirdPage);
+
+    var _this4 = _possibleConstructorReturn(this, (ThirdPage.__proto__ || Object.getPrototypeOf(ThirdPage)).call(this, props));
+
+    _this4.state = {
+      creditCard: '',
+      expiryDate: '',
+      cvv: '',
+      billingZC: ''
+    };
+    return _this4;
+  }
+
+  _createClass(ThirdPage, [{
+    key: 'handleChange',
+    value: function handleChange(event) {
+      this.setState(_defineProperty({}, event.target.className, event.target.value));
+    }
+
+    // handleClick(event) {
+    //   this.setState
+    // }
+
+  }, {
+    key: 'handleNavClick',
+    value: function handleNavClick(event) {
+      return this.props.click(event.target.className, this.state);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'holderThirdPage' },
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Credit Card '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'creditCard', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Expiry Date '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'expiryDate', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' CVV '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'cvv', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'css' },
+          ' Billing Zip '
+        ),
+        React.createElement(
+          'div',
+          { className: 'form' },
+          React.createElement('input', { type: 'text', className: 'billingZC', onChange: this.handleChange.bind(this) })
+        ),
+        React.createElement(
+          'div',
+          { className: 'navigator' },
+          React.createElement(
+            'span',
+            { className: 'previousBtn', onClick: this.handleNavClick.bind(this) },
+            ' Previous '
+          ),
+          React.createElement(
+            'span',
+            { className: 'submitBtn' },
+            ' Submit '
+          ),
+          React.createElement(
+            'span',
+            { className: 'nextBtn', onClick: this.handleNavClick.bind(this) },
+            ' Next '
+          )
+        )
+      );
+    }
+  }]);
+
+  return ThirdPage;
+}(React.Component);
+
+// -------------------------------------------------------------
+
+
+function ConfirmationPage(props) {
+  console.log(props);
   return React.createElement(
     'div',
-    { className: 'holderFirstPage' },
+    { className: 'holderConfirmationPage' },
     React.createElement(
       'div',
-      { className: 'name' },
-      'Name'
+      { className: 'css' },
+      ' Name '
     ),
     React.createElement(
       'div',
-      { className: 'email' },
-      'Email'
+      { className: 'cssConf' },
+      ' ',
+      props.name,
+      ' '
     ),
     React.createElement(
       'div',
-      { className: 'password' },
-      'Password'
+      { className: 'css' },
+      ' Email '
     ),
     React.createElement(
       'div',
-      { className: 'password' },
-      'Previous'
+      { className: 'cssConf' },
+      ' ',
+      props.email,
+      ' '
     ),
     React.createElement(
       'div',
-      { className: 'password' },
-      'Next'
+      { className: 'css' },
+      ' Password '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.password,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' Address '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.addressLine1,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.addressLine2,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' City '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.city,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' State '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.state,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' Zip '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.zipCode,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' Phone Number '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.phoneNumber,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' Credit Card Number '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.creditCard,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' Expiry Date '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.expiryDate,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' CVV '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.cvv,
+      ' '
+    ),
+    React.createElement(
+      'div',
+      { className: 'css' },
+      ' Billing Zip '
+    ),
+    React.createElement(
+      'div',
+      { className: 'cssConf' },
+      ' ',
+      props.billingZC,
+      ' '
     )
   );
 }
