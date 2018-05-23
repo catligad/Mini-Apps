43be30320e89c442c76d2d5c33b5601997d3c1c3
Catherine Ligad <kateligad@gmail.com>
2018-05-23 12:35:21 -0700

    finished setting up components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,3 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      checkoutPage: true,
+      firstPage: false,
+      secondPage: false,
+      thirdPage: false,
+      name:'',
+      email:'',
+      password:'',
+      addressLine1:'',
+      addressLine2:'',
+      city:'',
+      state:'',
+      zipCode:'',
+      phoneNumber:'',
+      creditCard:'',
+      expiryDate:'',
+      cvv:'',
+      billingZC:''
+    };
+  }
 
+  onCheckoutClick() {
+    this.setState({
+      checkoutPage: false,
+      firstPage:true,
+    });
+  }
+  
+  render(){
+    var pageRendered;
+    if (this.state.checkoutPage) {
+      pageRendered = <CheckoutPage click={this.onCheckoutClick.bind(this)} />
+    } else if (this.state.firstPage) {
+      pageRendered = <FirstPage />
+    } else if (this.state.secondPage) {
+      pageRendered = <SecondPage />
+    } else if (this.state.thirdPage) {
+      pageRendered = <Thirdpage />
+    }
+
+    return(
+      <div> 
+        {pageRendered}
+      </div>
+    )
+  }
+}
+
+function CheckoutPage (props) {
+  function clickHandler(){
+    console.log('I was clicked!')
+    return props.click();
+  };
+  return(
+    <div className="checkout" onClick={clickHandler.bind(this)}>
+    Checkout
+    </div>
+  )
+};
+
+function FirstPage (){
+  return(
+    <div className="holderFirstPage">
+    <div className="name"> 
+      Name
+    </div>
+    <div className="email"> 
+      Email
+    </div>
+    <div className="password"> 
+      Password
+    </div>
+    <div className="password"> 
+      Previous
+    </div>
+    <div className="password"> 
+      Next
+    </div>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Listening on 3000'));
 
