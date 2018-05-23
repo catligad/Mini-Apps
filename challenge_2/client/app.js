5301bba8241754b2a6cd3acba56a5b35286c81f5
Catherine Ligad <kateligad@gmail.com>
2018-05-22 17:27:28 -0700

    added dancing cats

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,6 +1,38 @@
 $(document).ready(function(){
+
+  // Snow Falling
+  function fallingSnow() {
+    
+    var $snowflakes = $(), qt = 20;
+    
+    for (var i = 0; i < qt; ++i) {
+        var $snowflake = $('<div class="snowflakes"><img src="http://gifdanceparty.giphy.com/assets/dancers/kitty.gif"></div>');
+        $snowflake.css({
+            'left': (Math.random() * $('#site').width()) + 'px',
+            'top': (- Math.random() * $('#site').height()) + 'px'
+        });
+        // add this snowflake to the set of snowflakes
+        $snowflakes = $snowflakes.add($snowflake);
+    }
+    $('#snowZone').prepend($snowflakes);
+
+    $snowflakes.animate({
+        top: "500px",
+        opacity : "1",
+    }, Math.random() + 5000, function(){
+        $(this).remove();
+    });
+}
+
+  $('#pic').click(() => {
+    fallingSnow();
+  })
+
   $('#title').click(() => {
-    $('#response').html('<img src="https://ninchanese.com/wp-content/uploads/sites/3/2013/03/Cat_puts_on_glasses_to_read_Chinese.gif" id="pic">')
+    $('#response').html('<img src="https://ninchanese.com/wp-content/uploads/sites/3/2013/03/Cat_puts_on_glasses_to_read_Chinese.gif" id="pic">');
+    $('#pic').click(() => {
+      fallingSnow();
+    });
   })
 
   $('#buttonSubmit').click(function() {
@@ -22,5 +54,7 @@ $(document).ready(function(){
       $('#response').html(data)
     });
   });
+
+
 
 });
