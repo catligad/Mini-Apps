f53ce797c5213cba0dc18ed960548a9feae826f3
Catherine Ligad <kateligad@gmail.com>
2018-05-24 12:03:31 -0700

    able to get hello world to show up on the client side from the server

--- challenge_4/client/src/app.jsx
+++ challenge_4/client/src/app.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Component } from 'react';
+// import { Component } from 'react';
 
 
-class App extends Component{
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+  }
 
+  render(){
+    return(
+      <div>Hello World!</div>
+    )
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
