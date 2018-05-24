da87fffd2fd62738076122ee81b78b04ac7c7c3a
Catherine Ligad <kateligad@gmail.com>
2018-05-24 11:49:53 -0700

    set up the server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var path = require('path');
 var parser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.listen(3000, () => console.log("I\'m listening!"));
+
